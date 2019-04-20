f9a5f5bc64b8e9332cbc539a30e0958257e2ea2e
Paul Matthew Washburn <paulmattheww@gmail.com>
2019-04-20 09:21:40 -0600

    Add eda.py skeleton and changes to README.md

--- src/auto_explore/eda.py
+++ src/auto_explore/eda.py
@@ -1,25 +1,8 @@
-# Auto Explore
+'''
+Class definitions for automated EDA.
+'''
 
-The goal of this Python library is to create a reliable tool for performing a first-pass exploratory data analysis.  Machine learning developers will hopefully benefit from a shortening iteration time for model production.  
 
-The earliest stages of a machine learning project require exploratory analysis to uncover raw features and insights that can be exploited in the modeling process.  Exploratory analysis typically follows a somewhat tree-like (and at times recursive) process where task-patterns emerge across projects.  By specifying certain parameters *a priori* about the data in question, a process that adheres to these task-patterns can be designed using open source tools to automate the majority of the "first pass" data analysis work -- freeing up time for deep-dive analyses, modeling, and deployment.
-
-The open source projects that will be relied upon most for this project include:
-
-    - [pandas-profiling](https://github.com/pandas-profiling/pandas-profiling)
-    - [featexp](https://github.com/abhayspawar/featexp)
-    - [speedml](https://speedml.com/automate-exploratory-data-analysis/)
-    - [pandas](https://pandas.pydata.org/)
-    - [dask](https://github.com/dask/dask)
-
-The delivery mechanism will be two-fold: a command line interface and a Python package usable in an interactive runtime environment.  Functionality will range from the bare minimum of feature identification to a more detailed reporting on the data.  A full suite will be available for patient users.  
-
-While the term "automated" data analysis sounds difficult, the heavy lifting has been done by these library authors.  This project will simply be extending good work that already exists, meaning I will not need to spend considerable time re-inventing the wheel on already established techniques.  
-
-A high-level design of the code is below.
-
-```python
-## auto-explore.py
 
 class AutopilotExploratoryAnalysis:
     '''
@@ -98,5 +81,3 @@ class AutopilotExploratoryAnalysis:
 
     def rf_feature_importance(self):
         pass
-
-```
