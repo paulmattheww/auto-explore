5d09549ce2d06990d0f6c66a1a39a55900fbad54
washburp <paul.washburn@pinnacol.com>
2019-04-29 11:39:16 -0600

    Bump version: 0.1.0 → 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'Auto Explore'
 year = '2019'
 author = 'Paul Matthew Washburn'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.1.0'
+version = release = '0.1.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
